985eede4637a8d251b944bfd2e6a82a8e631930f
Fosmos <erme2390@gmail.com>
2021-06-15 15:16:46 -0400

    Bug Fixes and Cleanups

--- run_simulation.sh
+++ run_simulation.sh
@@ -10,26 +10,27 @@ Main()
 
     # Defining directory to save results
     # Always creates a new directory instead of replacing a previous one
-    declare -i index=1
+    declare -i RUN_INDEX=1
     while true; do
         # Creates a new run
-        if [[ ! -d "${VISUALIZATION_DIR}/run${index}" ]]; then
-            VISUALIZATION_DIR="${VISUALIZATION_DIR}run${index}"
+        if [[ ! -d "${VISUALIZATION_DIR}run${RUN_INDEX}" ]]; then
+            VISUALIZATION_DIR="${VISUALIZATION_DIR}run${RUN_INDEX}"
             break;
         fi
-        index=$(( index + 1 ))
+        RUN_INDEX=$((RUN_INDEX + 1))
     done
 
     # Make directories if they don't exist
     mkdir -p Scripts/Input_Generator/output
     mkdir -p Scripts/Msg_Log_Parser/input
     mkdir -p Scripts/Msg_Log_Parser/output
-    mkdir -p ${VISUALIZATION_DIR}
+    mkdir -p $VISUALIZATION_DIR
 
     # Generate a scenario json file for model input, save it in the config folder
     echo "Generating Scenario:"
     cd Scripts/Input_Generator
     python3 generate_${AREA_FILE}_json.py
+    ErrorCheck $? # Check for build errors
     cp output/scenario_${AREA_FILE}.json ../../config
     cd ../..
 
@@ -37,35 +38,18 @@ Main()
     cd bin
     echo
     echo "Executing model:"
-    echo ${SIMULATE}
-    ${SIMULATE}
-
-    # Catch any build errors
-    if [ "$?" -ne 0 ]; then
-        cd ..
-        echo -e "\033[31mBuild Failed\033[0m"
-        exit -1 # And exit if any exist
-    fi
+    echo $SIMULATE
+    $SIMULATE
+    ErrorCheck $? log # Check for build errors
 
     # Generate SIRDS graphs
     echo
     echo "Generating graphs and stats (will be found in logs folder):"
     cd ../Scripts/Graph_Generator/
     python3 graph_per_regions.py
-
-    if [ "$?" -ne 0 ]; then
-        cd ../..
-        echo -e "\033[31mBuild Failed\033[0m"
-        exit -1 # And exit if any exist
-    fi
-
+    ErrorCheck $? # Check for build errors
     python3 graph_aggregates.py
-    if [ "$?" -ne 0 ]; then
-        cd ../..
-        echo -e "\033[31mBuild Failed\033[0m"
-        exit -1 # And exit if any exist
-    fi
-
+    ErrorCheck $? # Check for build errors
     cd ../..
 
     # Copy the message log + scenario to message log parser's input
@@ -82,34 +66,35 @@ Main()
     # Run the message log parser
     echo "Running message log parser:"
     cd Scripts/Msg_Log_Parser
-    echo ${PARSE_MSG_LOGS}
-    ${PARSE_MSG_LOGS}
+    echo $PARSE_MSG_LOGS
+    $PARSE_MSG_LOGS > log 2>&1
+    ErrorCheck $? log # Check for build errors
     echo
     unzip "output\pandemic_messages.zip" -d output
     cd ../..
 
     # Copy the converted message logs to GIS Web Viewer Folder
     echo
-    echo "Copying converted files to: ${VISUALIZATION_DIR}"
-    cp Scripts/Msg_Log_Parser/output/messages.log ${VISUALIZATION_DIR}
-    cp Scripts/Msg_Log_Parser/output/structure.json ${VISUALIZATION_DIR}
+    echo "Copying converted files to: $VISUALIZATION_DIR"
+    cp Scripts/Msg_Log_Parser/output/messages.log $VISUALIZATION_DIR
+    cp Scripts/Msg_Log_Parser/output/structure.json $VISUALIZATION_DIR
     if [[ $AREA == "ottawa" ]]; then
-        cp GIS_Viewer/${AREA}/ottawaDA.geojson ${VISUALIZATION_DIR}
+        cp GIS_Viewer/${AREA}/ottawaDA.geojson $VISUALIZATION_DIR
     else
-        cp GIS_Viewer/${AREA}/${AREA_FILE}.geojson ${VISUALIZATION_DIR}; fi
-    cp GIS_Viewer/${AREA}/visualization.json ${VISUALIZATION_DIR}
+        cp GIS_Viewer/${AREA}/${AREA_FILE}.geojson $VISUALIZATION_DIR; fi
+    cp GIS_Viewer/${AREA}/visualization.json $VISUALIZATION_DIR
 
-    finishTime=$SECONDS
+    BUILD_TIME=$SECONDS
     echo -en "\033[32mSimulation Complete and it took "
-    if [[ $finishTime -ge 3600 ]]; then
-        echo -en "$((finishTime / 3600))h";
-        finishTime=$(($finishTime % 3600))
+    if [[ $BUILD_TIME -ge 3600 ]]; then
+        echo -en "$((BUILD_TIME / 3600))h";
+        BUILD_TIME=$((BUILD_TIME % 3600))
     fi
-    if [[ $finishTime -ge 60 ]]; then echo -en "$(($finishTime / 60))m"; fi
-    if [[ $(($finishTime % 60)) > 0 ]]; then echo -en "$(($finishTime % 60))s"; fi
+    if [[ $BUILD_TIME -ge 60 ]]; then echo -en "$((BUILD_TIME / 60))m"; fi
+    if [[ $((BUILD_TIME % 60)) > 0 ]]; then echo -en "$((BUILD_TIME % 60))s"; fi
     echo -e " to complete\033[0m"
 
-    echo -e "View results using the files in \033[1;32mrun1\033[0m and this web viewer: \033[1;36mhttp://206.12.94.204:8080/arslab-web/1.3/app-gis-v2/index.html\033[0m"
+    echo -e "View results using the files in \033[1;32mrun${RUN_INDEX}\033[0m and this web viewer: \033[1;36mhttp://206.12.94.204:8080/arslab-web/1.3/app-gis-v2/index.html\033[0m"
 }
 
 # Helps clean up past simulation runs
@@ -118,7 +103,7 @@ Clean()
     # Delete all the sims for the selected area if no number specified
     if [[ $RUN == -1 ]]; then
         echo -e "Removing \033[33mall\033[0m runs for \033[33m${AREA}\033[31m"
-        rm -rfv ${VISUALIZATION_DIR}
+        rm -rfv $VISUALIZATION_DIR
     # Otherwise delete the run that matches the number passed in
     else
         echo -e "Removing \033[33mrun${RUN}\033[0m for \033[33m${AREA}\033[31m"
@@ -126,6 +111,16 @@ Clean()
     fi
 
     echo -en "\033[0m" # Reset the colors
+}
+
+ErrorCheck()
+{
+    # Catch any build errors
+    if [[ "$1" -ne 0 ]]; then
+        if [[ "$2" == "log" ]]; then cat $2; fi # Print any error messages
+        echo -e "\033[31mBuild Failed\033[0m"
+        exit -1 # And exit if any exist
+    fi
 }
 
 # Displays the help
@@ -199,21 +194,15 @@ else
     if [[ ! -f "bin/pandemic-geographical_model" ]]; then
         cmake CMakeLists.txt
         make > log 2>&1
-
-        # Catch any build errors
-        if [ "$?" -ne 0 ]; then
-            cat log
-            echo -e "\033[31mBuild Failed\033[0m"
-            exit -1 # And exit if any exist
-        fi
+        ErrorCheck $? log # Check for build errors
 
         echo -e "\033[32mBuild Completed\033[0m"
 
-        if [[ ${AREA} == "" && ${AREA_FILE} == "" ]]; then exit 1; fi
+        if [[ $AREA == "" && $AREA_FILE == "" ]]; then exit 1; fi
     fi
 
     # If not are is set or is set incorrectly, then exit
-    if [[ ${AREA} == "" || ${AREA_FILE} == "" ]]; then echo -e "\033[31mPlease set a valid area flag..\033[0m Use \033[33m--flags\033[0m to see them"; exit -1; fi
+    if [[ $AREA == "" || $AREA_FILE == "" ]]; then echo -e "\033[31mPlease set a valid area flag..\033[0m Use \033[33m--flags\033[0m to see them"; exit -1; fi
 
     # Used both in Clean() and Main() so we set it here
     VISUALIZATION_DIR="GIS_Viewer/${AREA}/simulation_runs/"
