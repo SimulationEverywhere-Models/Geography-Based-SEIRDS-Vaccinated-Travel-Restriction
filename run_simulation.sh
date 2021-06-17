91b950a281114510cbe942f10d9e556a07af8430
Fosmos <erme2390@gmail.com>
2021-06-17 11:47:47 -0400

    Tweaks and Cleanups

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,5 +1,16 @@
 # Written by Eric and based on scripts made by Glenn
-# This script compiles the model (if it is not already) and assumes the environment running this script includes python and python geopandas
+# This script compiles the model and assumes the environment running this script includes python and python geopandas
+
+# <Colors>  #
+    RESET="\033[0m"
+    RED="\033[31m"
+    GREEN="\033[32m"
+    YELLOW="\033[33m"
+    BLUE="\033[36m"
+    BOLD="\033[1m"
+    ITALIC="\033[3m"
+
+# </Colors> #
 
 # Runs the model and saves the results in the GIS_Viewer directory
 Main()
@@ -22,8 +33,6 @@ Main()
 
     # Make directories if they don't exist
     mkdir -p Scripts/Input_Generator/output
-    mkdir -p Scripts/Msg_Log_Parser/input
-    mkdir -p Scripts/Msg_Log_Parser/output
     mkdir -p $VISUALIZATION_DIR
 
     # Generate a scenario json file for model input, save it in the config folder
@@ -38,15 +47,13 @@ Main()
 
     # Run the model
     cd bin
-    echo
-    echo "Executing model:"
+    echo; echo "Executing model:"
     echo $SIMULATE
     $SIMULATE
     ErrorCheck $? # Check for build errors
 
     # Generate SIRDS graphs
-    echo
-    echo "Generating graphs and stats (will be found in logs folder):"
+    echo; echo "Generating graphs and stats (will be found in logs folder):"
     cd ../Scripts/Graph_Generator/
     python3 graph_per_regions.py
     ErrorCheck $? # Check for build errors
@@ -56,12 +63,9 @@ Main()
 
     # Copy the message log + scenario to message log parser's input
     # Note this deletes the contents of input/output folders of the message log parser before executing
-    echo
-    echo "Copying simulation results to message log parser:"
-    rm -f Scripts/Msg_Log_Parser/input/*
-    rm -rf Scripts/Msg_Log_Parser/output/pandemic_messages
-    rm -f Scripts/Msg_Log_Parser/*.zip
-    rm -f Scripts/Msg_Log_Parser/output/*
+    echo; echo "Copying simulation results to message log parser:"
+    mkdir -p Scripts/Msg_Log_Parser/input
+    mkdir -p Scripts/Msg_Log_Parser/output
     cp config/scenario_${AREA_FILE}.json Scripts/Msg_Log_Parser/input
     cp logs/pandemic_messages.txt Scripts/Msg_Log_Parser/input
 
@@ -71,29 +75,30 @@ Main()
     echo $PARSE_MSG_LOGS
     $PARSE_MSG_LOGS > log 2>&1
     ErrorCheck $? log # Check for build errors
-    echo
     unzip "output\pandemic_messages.zip" -d output
     cd ../..
 
     # Copy the converted message logs to GIS Web Viewer Folder
-    echo
-    echo "Copying converted files to: $VISUALIZATION_DIR"
-    cp Scripts/Msg_Log_Parser/output/messages.log $VISUALIZATION_DIR
-    cp Scripts/Msg_Log_Parser/output/structure.json $VISUALIZATION_DIR
+    echo; echo; echo -e "Copying converted files to: ${BLUE}${VISUALIZATION_DIR}${RESET}"
+    mv Scripts/Msg_Log_Parser/output/messages.log $VISUALIZATION_DIR
+    mv Scripts/Msg_Log_Parser/output/structure.json $VISUALIZATION_DIR
+    rm -rf Scripts/Msg_Log_Parser/input
+    rm -rf Scripts/Msg_Log_Parser/output
+    rm -f Scripts/Msg_Log_Parser/*.zip
     cp GIS_Viewer/${AREA}/${AREA_FILE}.geojson $VISUALIZATION_DIR
     cp GIS_Viewer/${AREA}/visualization.json $VISUALIZATION_DIR
 
     BUILD_TIME=$SECONDS
-    echo -en "\033[32mSimulation Complete and it took "
+    echo -en "${GREEN}Simulation Complete and it took "
     if [[ $BUILD_TIME -ge 3600 ]]; then
-        echo -en "$((BUILD_TIME / 3600))h";
+        echo -en "$((BUILD_TIME / 3600))h"
         BUILD_TIME=$((BUILD_TIME % 3600))
     fi
     if [[ $BUILD_TIME -ge 60 ]]; then echo -en "$((BUILD_TIME / 60))m"; fi
     if [[ $((BUILD_TIME % 60)) > 0 ]]; then echo -en "$((BUILD_TIME % 60))s"; fi
-    echo -e " to complete\033[0m"
+    echo -e " to complete${RESET}"
 
-    echo -e "View results using the files in \033[1;32mrun${RUN_INDEX}\033[0m and this web viewer: \033[1;36mhttp://206.12.94.204:8080/arslab-web/1.3/app-gis-v2/index.html\033[0m"
+    echo -e "View results using the files in ${BOLD}${BLUE}run${RUN_INDEX}${RESET} and this web viewer: ${BOLD}${BLUE}http://206.12.94.204:8080/arslab-web/1.3/app-gis-v2/index.html${RESET}"
 }
 
 # Helps clean up past simulation runs
@@ -101,15 +106,15 @@ Clean()
 {
     # Delete all the sims for the selected area if no number specified
     if [[ $RUN == -1 ]]; then
-        echo -e "Removing \033[33mall\033[0m runs for \033[33m${AREA}\033[31m"
+        echo -e "Removing ${YELLOW}all${RESET} runs for ${YELLOW}${AREA}${RED}"
         rm -rfv $VISUALIZATION_DIR
     # Otherwise delete the run that matches the number passed in
     else
-        echo -e "Removing \033[33mrun${RUN}\033[0m for \033[33m${AREA}\033[31m"
+        echo -e "Removing ${YELLOW}run${RUN}${RESET} for ${YELLOW}${AREA}${RED}"
         rm -rfdv ${VISUALIZATION_DIR}run${RUN}
     fi
 
-    echo -en "\033[0m" # Reset the colors
+    echo -en $RESET # Reset the colors
 }
 
 ErrorCheck()
@@ -117,29 +122,31 @@ ErrorCheck()
     # Catch any build errors
     if [[ "$1" -ne 0 ]]; then
         if [[ "$2" == "log" ]]; then cat $2; fi # Print any error messages
-        echo -e "\033[31mBuild Failed\033[0m"
+        echo -e "${RED}Build Failed${RESET}"
         exit -1 # And exit if any exist
     fi
+
+    if [[ -f "log" ]]; then rm -f log; fi
 }
 
 # Displays the help
 Help()
 {
     if [[ $1 == 1 ]]; then
-        echo -e "\033[1mFlags:\033[0m"
-        echo -e " \033[33m--clean|-c|--clean=#|-c=#\033[0m \t Cleans all simulation runs for the selected area if no # is set, \n \t\t\t\t otherwise cleans the specified run using the # inputed such as 'clean=1'"
-        echo -e " \033[33m--flags, -f\033[0m \t\t\t Displays all flags"
-        echo -e " \033[33m--help, -h\033[0m \t\t\t Displays the help"
-        echo -e " \033[33m--Ontario, --ontario, -On, -on\033[0m  Runs a simulation in Ontario"
-        echo -e " \033[33m--Ottawa, --ottawa, -Ot, -ot\033[0m \t Runs a simulation in Ottawa"
-        echo -e " \033[33m--rebuild, -r\033[0m \t\t\t Rebuilds the model"
-        echo -e " \033[33m--no-progress, -np\033[0m \t\t Turns off the progress bars and loading animations"
+        echo -e "${YELLOW}Flags:${RESET}"
+        echo -e " ${YELLOW}--clean|-c|--clean=#|-c=#${RESET} \t Cleans all simulation runs for the selected area if no # is set, \n \t\t\t\t otherwise cleans the specified run using the # inputed such as 'clean=1'"
+        echo -e " ${YELLOW}--flags, -f${RESET}\t\t\t Displays all flags"
+        echo -e " ${YELLOW}--help, -h${RESET}\t\t\t Displays the help"
+        echo -e " ${YELLOW}--Ontario, --ontario, -On, -on${RESET} Runs a simulation in Ontario"
+        echo -e " ${YELLOW}-Ottawa, --ottawa, -Ot, -ot${RESET}\t Runs a simulation in Ottawa"
+        echo -e " ${YELLOW}--rebuild, -r${RESET}\t\t\t Rebuilds the model"
+        echo -e " ${YELLOW}--no-progress, -np${RESET}\t\t Turns off the progress bars and loading animations"
     else
-        echo -e "\033[1mUsage:\033[0m"
-        echo -e " ./run_simulation.sh \033[3m<area flag>\033[0m"
-        echo -e " where \033[3m<area flag>\033[0m is either --Ottawa \033[1mOR\033[0m --Ontario"
+        echo -e "${BOLD}Usage:${RESET}"
+        echo -e " ./run_simulation.sh ${ITALIC}<area flag>${RESET}"
+        echo -e " where ${ITALIC}<area flag>${RESET}is either --Ottawa ${BOLD}OR${RESET}--Ontario"
         echo -e " example: ./run_simulation.sh --Ottawa"
-        echo -e "Use \033[1;33m--flags\033[0m to see a list of all the flags and their meanings"
+        echo -e "Use \033[1;33m--flags${RESET}to see a list of all the flags and their meanings"
     fi
 }
 
@@ -187,7 +194,7 @@ else
                 shift
             ;;
             *)
-                echo -e "\033[31mUnknown parameter: \033[33m${1}\033[0m"
+                echo -e "${RED}Unknown parameter: ${YELLOW}${1}${RESET}"
                 Help;
                 exit -1;
             ;;
@@ -200,13 +207,13 @@ else
         make > log 2>&1
         ErrorCheck $? log # Check for build errors
 
-        echo -e "\033[32mBuild Completed\033[0m"
+        echo -e "${GREEN}Build Completed${RESET}"
 
         if [[ $AREA == "" && $AREA_FILE == "" ]]; then exit 1; fi
     fi
 
     # If not are is set or is set incorrectly, then exit
-    if [[ $AREA == "" || $AREA_FILE == "" ]]; then echo -e "\033[31mPlease set a valid area flag..\033[0m Use \033[33m--flags\033[0m to see them"; exit -1; fi
+    if [[ $AREA == "" || $AREA_FILE == "" ]]; then echo -e "${RED}Please set a valid area flag..${RESET}Use ${YELLOW}--flags${RESET}to see them"; exit -1; fi
 
     # Used both in Clean() and Main() so we set it here
     VISUALIZATION_DIR="GIS_Viewer/${AREA}/simulation_runs/"
